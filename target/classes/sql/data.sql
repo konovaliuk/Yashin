aab8a6546810bad76aed055e50d9e96fc1bed350
andrewyashin <razzer9876@gmail.com>
2017-05-22 00:36:26 +0300

    Delete target/

--- target/classes/sql/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `railway_system`.`price` (`berth_factor`, `compartment_factor`, `deluxe_factor`) VALUES ('0.2', '0.75', '1');
-INSERT INTO `railway_system`.`price` (`berth_factor`, `compartment_factor`, `deluxe_factor`) VALUES ('0.5', '1', '1.5');
-INSERT INTO `railway_system`.`price` (`berth_factor`, `compartment_factor`, `deluxe_factor`) VALUES ('1', '2', '3');
-
-
